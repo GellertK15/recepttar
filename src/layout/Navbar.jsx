76e8cde2fb79cc076ac027e3bd88957de1542bf9
GellertK15 <kiraly.gellert@gmail.com>
2026-01-09 11:57:34 +0100

    recepttar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,19 +3,22 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <li className="nav-item">
-                <NavLink to="/" className="nav-link">Home</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/receptek" className="nav-link">Recept lista</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/kereso" className="nav-link">Kereső</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/admin" className="nav-link">Admin</NavLink>
-            </li>
+            <ul className="nav">
+                <li className="nav-item">
+                    <NavLink to="/" className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/receptek" className="nav-link">Recept lista</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/kereso" className="nav-link">Kereső</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/admin" className="nav-link">Admin</NavLink>
+                </li>
+            </ul>
         </nav>
-    )
-}
+    );
+};
+
 export default Navbar;
